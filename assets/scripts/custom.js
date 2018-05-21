74e2a64e4fd82135c8535c8261372827f27c52b8
Liepa <39078129+liepavai@users.noreply.github.com>
2018-05-21 17:30:41 +0300

    Add files via upload

---
+++ assets/scripts/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+
+
+$('.grid').masonry({
+  // options
+	itemSelector: '.grid-item',
+	columnWidth: ".grid-item"
+});
+
+var elements = $('.sticky');
+// Stickyfill.add(elements);
+
+});
---
+++ assets/scripts/menu.js
@@ -0,0 +1,9 @@
+console.log(0);
+$(document).ready(function(){
+console.log(1);
+
+	$('.burger-nav').click(function(){
+console.log(2);
+		$('.menu').toggleClass('open');
+	});
+});
